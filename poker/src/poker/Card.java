26efed754c34fd995fcc4b998b7ab417cd2f0323
sisasam <34127885+sisasam@users.noreply.github.com>
2018-05-06 00:32:32 +0200

    Class Card
    Neue boolean Eigenschaft imDeck

--- poker/src/poker/Card.java
+++ poker/src/poker/Card.java
@@ -1,9 +1,15 @@
 package poker;
 
-public class Card {
-
+public class Card 
+{
+	/**
+	 * Card Element erstellt eine Karte der Pokerspiels
+	 * Eigenschaften der Farbe und des Werts der jeweiligen Karte
+	 * 	imDeck zeigt an, ob die Karte sich noch im Deck befindet oder schon gezogen wurde
+	 */
 	private String farbe;
 	private String wert;
+	private boolean imDeck;
 	
 	public String getFarbe() 
 	{
@@ -13,6 +19,8 @@ public class Card {
 	{
 		this.farbe = farbe;
 	}
+	
+	
 	public String getWert() 
 	{
 		return wert;
@@ -22,15 +30,28 @@ public class Card {
 		this.wert = wert;
 	}
 	
+	
+	@Override
 	public String toString() 
 	{
 		return farbe + " " + wert;
 	}
+
+	public boolean getImDeck() 
+	{
+		return imDeck;
+	}
+	public void setImDeck(boolean imDeck) 
+	{
+		this.imDeck = imDeck;
+	}
+	
+	
 	
 	public static void main(String[] args) 
 	{
-
-
+		
 	}
+	
 
 }
